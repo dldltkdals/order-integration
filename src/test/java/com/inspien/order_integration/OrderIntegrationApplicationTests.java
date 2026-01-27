ba1d981f3b22f4ed4ec3cefbcc50e7d89f0aee2e
daum0604 <daum0604@naver.com>
2026-01-27 23:11:00 +0900

    Initial commit

---
+++ src/test/java/com/inspien/order_integration/OrderIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspien.order_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
